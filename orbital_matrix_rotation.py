a005e1dd2b2a21da8d4facecd793ecf704f02377
Zhang Anjun <admin@zhanganjun.com>
2025-05-22 14:47:44 +0800

    v2.0

--- orbital_matrix_rotation.py
+++ orbital_matrix_rotation.py
@@ -1,61 +1,59 @@
 # File Name: orbital_matrix_rotation.py
 # Author: Zhang Anjun
-# Date: 2025-05-21
-# Version: 1.2
+# Date: 2025-05-22
+# Version: 2.0
 # © 2025 Zhang Anjun. All rights reserved.
 
 from sys import exit as sysExit
 
+def copyrightNotice():
+    print("")
+    print("Author: Zhang Anjun")
+    print("Version: 2.0")
+    print("© 2025 Zhang Anjun. All rights reserved.")
+    print("")
+
 def orbitalRotation(matrix, n, step):
-    step = modStep(n, step)
-    absStep = step * direction(step)
-    if step == 0:
-        return matrix
+    direction = sign(step)                                  # Find rotation direction
+    step = modStep(n, step)                                 # Avoid full-circle rotations
     i = 0
-    while i != absStep:
-        row, col = n, n
-        latest = matrix[row][col]
-        j = 0
-        while j != calCircumference(n):
-            nextRow, nextCol = orbit(n, step, row, col)
-            matrix[nextRow][nextCol], latest = latest, matrix[nextRow][nextCol]
-            row, col = nextRow, nextCol
-            j = j + 1
+    while i != step:                                        # Move multiple steps
+        matrix = rotateOnePlace(matrix, n, direction)       # Move one place each time
         i = i + 1
     return matrix
 
-def calCircumference(n):
-    side = 7 - (2 * n)
-    circumference = (4 * side) - 4
-    return circumference
-
-def modStep(n, step):
-    sign = direction(step)
-    absStep = sign * step
-    circumference = calCircumference(n)
-    step = (absStep % circumference) * sign
-    return step
-
-def nextSwapPosition(n, step, row, col):
-    while step != 0:
-        row, col = orbit(n, step, row, col)
-        step = step - direction(step)
-    return row, col
-
-def orbit(n, step, row, col):                                       # Move 1 place clockwise/anti-clockwise. Use a loop to control moving
-    if direction(step) == 1:
-        row, col = clockwiseRotation(n, row, col)
-    elif direction(step) == -1:
-        row, col = anticlockwiseRotation(n, row, col)
-    return row, col
-
-def direction(step):                                                # Determine whether clockwise or anti-clockwise
+def sign(step):
     if step >= 0:
         return 1
     elif step < 0:
         return -1
 
-def clockwiseRotation(n, row, col):                                 # Move 1 place clockwise
+def modStep(n, step):
+    circumference = getCircumference(n)
+    if step < 0:
+        step = step * (-1)                                  # Keep the "step" positive
+    step = step % circumference                             # Reduce by full rotations
+    return step
+
+def rotateOnePlace(matrix, n, direction):                   # Move one place clockwise/anti-clockwise
+    row, col = n, n                                         # Start at the top-left corner
+    latest = matrix[row][col]
+    j = 0
+    while j != getCircumference(n):
+        if direction == 1:                                  # Find next clockwise position
+            row, col = nextClockwise(n, row, col)
+        elif direction == -1:                               # Find next counterclockwise position
+            row, col = nextCounterclockwise(n, row, col)
+        matrix[row][col], latest = latest, matrix[row][col] # Place "latest" into new position. Take its old value into "latest"
+        j = j + 1
+    return matrix
+
+def getCircumference(n):
+    side = 7 - (2 * n)
+    circumference = (4 * side) - 4
+    return circumference
+
+def nextClockwise(n, row, col):                             # Find next clockwise position
     if row == n and col < 6 - n:
         col = col + 1
     elif row < 6 - n and col == 6 - n:
@@ -66,7 +64,7 @@ def clockwiseRotation(n, row, col):                                 # Move 1 pla
         row = row - 1
     return row, col
 
-def anticlockwiseRotation(n, row, col):                             # Move 1 place anti-clockwise
+def nextCounterclockwise(n, row, col):                      # Find next counterclockwise position
     if row == n and col > n:
         col = col - 1
     elif row < 6 - n and col == n:
@@ -77,7 +75,7 @@ def anticlockwiseRotation(n, row, col):                             # Move 1 pla
         row = row - 1
     return row, col
 
-def prompt():
+def prompt():                                               # Get user input
     print("Matrix before rotation: ")
     matrix = [
         [1, 2, 3, 4, 5, 6, 7],
@@ -96,16 +94,13 @@ def prompt():
 
 def printMatrix(matrix):
     row = 0
-    while row != 7:
-        print(matrix[row])
+    while row < 7:
+        col = 0
+        while col < 7:
+            print(str(matrix[row][col]) + " ", end="")
+            col = col + 1
+        print("")
         row = row + 1
-
-def copyrightNotice():
-    print("")
-    print("Author: Zhang Anjun")
-    print("Version: 1.2")
-    print("© 2025 Zhang Anjun. All rights reserved.")
-    print("")
 
 matrix, n, step = prompt()
 orbitalRotation(matrix, n, step)
